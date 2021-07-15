20be3a039f516bcafc71152a6afac592135472f9
shahin <vatharishok@gmail.com>
2021-07-15 23:52:32 +0430

    added the Dynamic filtering for fields

--- src/main/java/com/shahin/restfulwebservices/models/SomeBean.java
+++ src/main/java/com/shahin/restfulwebservices/models/SomeBean.java
@@ -1,14 +1,13 @@
 package com.shahin.restfulwebservices.models;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-//we can also Use this One
-@JsonIgnoreProperties(value={"field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
     private String field2;
-    @JsonIgnore
     private String field3;
 
     public SomeBean(String value1, String value2, String value3) {
