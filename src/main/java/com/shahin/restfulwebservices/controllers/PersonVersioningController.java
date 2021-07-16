a4b531b481642a25f2672671e635cfb91599e40a
shahin <vatharishok@gmail.com>
2021-07-17 00:12:03 +0430

    Versioning RESTful Services - Header and Content Negotiation Approach

--- src/main/java/com/shahin/restfulwebservices/controllers/PersonVersioningController.java
+++ src/main/java/com/shahin/restfulwebservices/controllers/PersonVersioningController.java
@@ -9,13 +9,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-    @GetMapping("v1/person")
+    @GetMapping(value="person/param", params="version=1")
     public PersonV1 personV1(){
         return new PersonV1("Ramtin The Cuzz");
     }
 
-    @GetMapping("v2/person")
+    @GetMapping(value="person/param",params="version=2")
     public PersonV2 personV2(){
+        return new PersonV2(new Name("Ram","Cuzz"));
+    }
+    @GetMapping(value="person/header", headers="X-API-VERSION=1")
+    public PersonV1 headerV1(){
+        return new PersonV1("Ramtin The Cuzz");
+    }
+
+    @GetMapping(value="person/header",headers="X-API-VERSION=2")
+    public PersonV2 headerV2(){
+        return new PersonV2(new Name("Ram","Cuzz"));
+    }
+    @GetMapping(value="person/produces", produces = "application/shk.company.app-v1+xml")
+    public PersonV1 producesV1(){
+        return new PersonV1("Ramtin The Cuzz");
+    }
+
+    @GetMapping(value="person/produces",produces = "application/shk.company.app-v2+json")
+    public PersonV2 producesV2(){
         return new PersonV2(new Name("Ram","Cuzz"));
     }
 
