a761391dba603fbe10a88ff73b7d91c03d6c4869
shahin <vatharishok@gmail.com>
2021-07-20 18:57:42 +0430

    Role Based Authentication

--- src/main/java/com/shahin/restfulwebservices/configuration/security/ApplicationSecurityConfig.java
+++ src/main/java/com/shahin/restfulwebservices/configuration/security/ApplicationSecurityConfig.java
@@ -45,8 +45,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
-                .permitAll()
+                .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers("/api/users/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
