e1b12e58f8066f823590c73c2809f97345b8e85e
sh.khalajestanii <NiH@sh123>
2021-08-03 14:15:26 +0430

    Refactored Security Settings For Swagger

--- src/main/java/com/shahin/restfulwebservices/configuration/security/ApplicationSecurityConfig.java
+++ src/main/java/com/shahin/restfulwebservices/configuration/security/ApplicationSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,6 +40,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring().
+
+                regexMatchers("^(?!/webjars/).*").
+                regexMatchers("^(?!/swagger-resources/).*").
+                regexMatchers("^(?!/swagger-ui.html/).*");
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(daoAuthenticationProvider());
     }
@@ -58,6 +69,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                         JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers("/login").permitAll()
                 .anyRequest()
                 .authenticated();
     }
